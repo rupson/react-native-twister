b5725665ec2adfc7791491297e353b35d0bc3da9
rupson <44114899+rupson@users.noreply.github.com>
2020-07-05 12:53:47 +0100

    feat(players): can edit the players list

--- components/PlayerListItem.tsx
+++ components/PlayerListItem.tsx
@@ -1,12 +1,59 @@
-import React from "react";
-import { List } from "react-native-paper";
+import React from 'react';
+import { List, IconButton, TextInput } from 'react-native-paper';
 
 interface PlayerListItemProps {
     name: string;
+    index: number;
+    removeFromList: (value: any) => any;
+    editValue: (index: number, newValue: string) => any;
 }
 
-const Item: React.FC<PlayerListItemProps> = ({ name }) => (
-    <List.Item title={name} style={{ width: "100%" }} accessibilityStates />
-);
+const Item: React.FC<PlayerListItemProps> = ({
+    name,
+    index,
+    removeFromList,
+    editValue,
+}) => {
+    const [_name, setName] = React.useState<string>(name);
+    const [editing, setEditing] = React.useState<boolean>(false);
+    return !editing ? (
+        <List.Item
+            title={name}
+            style={{
+                width: '100%',
+                borderBottomColor: '#e5e5e5',
+                borderBottomWidth: 1,
+            }}
+            right={() => (
+                <>
+                    <IconButton
+                        icon={'playlist-edit'}
+                        onPress={() => setEditing(true)}
+                        accessibilityStates
+                    />
+
+                    <IconButton
+                        icon={'minus-circle-outline'}
+                        onPress={() => removeFromList(index)}
+                        accessibilityStates
+                    />
+                </>
+            )}
+            accessibilityStates
+        />
+    ) : (
+        <TextInput
+            mode={'outlined'}
+            value={_name}
+            onBlur={() => setEditing(false)}
+            onChangeText={(text) => setName(text)}
+            autoFocus={true}
+            onSubmitEditing={({ nativeEvent: { text } }) => {
+                editValue(index, text);
+            }}
+            accessibilityStates
+        />
+    );
+};
 
 export default Item;
